8087dc3c125a26e0cf255502a15a2cb5b83aa74b
KilZenc <chchan.twenty@gmail.com>
2021-01-26 22:51:11 +0700

    Add initialize mod and reload config

---
+++ FishingAssistant/ModEntry.cs
@@ -0,0 +1,44 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishingAssistant
+{
+    partial class ModEntry : Mod
+    {
+        private ModConfig Config;
+
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            // Initialize mod
+            Initialize(helper);
+        }
+
+        /// <summary> Raised after the game state is updated (≈60 times per second). </summary>
+        private void OnUpdateTicked(object sender, UpdateTickedEventArgs e)
+        {
+
+        }
+
+        /// <summary>  Raised after a game menu is opened, closed, or replaced. </summary>
+        private void OnMenuChanged(object sender, MenuChangedEventArgs e)
+        {
+
+        }
+
+        /// <summary> Raised after the player presses a button on the keyboard, controller, or mouse. </summary>
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            //Reload new config
+            ReloadConfig(e);
+        }
+    }
+}
---
+++ FishingAssistant/SModEntry.cs
@@ -0,0 +1,35 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishingAssistant
+{
+    partial class ModEntry : Mod
+    {
+        private void Initialize(IModHelper helper)
+        {
+            Config = helper.ReadConfig<ModConfig>();
+
+            helper.Events.GameLoop.UpdateTicked += OnUpdateTicked;
+            helper.Events.Display.MenuChanged += OnMenuChanged;
+            helper.Events.Input.ButtonPressed += OnButtonPressed;
+
+            Monitor.Log("Initialized (press F5 to reload config)", LogLevel.Info);
+        }
+
+        private void ReloadConfig(ButtonPressedEventArgs e)
+        {
+            if (e.Button == SButton.F5)
+            {
+                Config = Helper.ReadConfig<ModConfig>();
+                Monitor.Log("Config reloaded", LogLevel.Info);
+            }
+        }
+    }
+}
